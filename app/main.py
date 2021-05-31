daac3f68047d830954829460578326bc4873e00d
Niklas Scheidthauer <niklas.scheidthauer@generali.com>
2021-05-31 09:52:18 +0200

    Sourced logging out to caller, Improved Docs

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from typing import Union
 
 from fastapi import Depends, FastAPI, HTTPException, Response, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from starlette.responses import RedirectResponse
 
 from app.docker_deployment import DockerDeployment
 from app.models import DeployModelInput, RuntimeEnv, RuntimeEnvType, UndeployModelInput
@@ -11,7 +12,7 @@ from app.tmux_deployment import TmuxDeployment
 app = FastAPI(
     title='BentoML Deployment Coordinizer',
     description='A webservice that provides endpoints to manage ML-model deployments via BentoML in tmux-sessions or Docker containers',
-    version='0.1',
+    version='1.0',
 )
 
 security = HTTPBasic()
@@ -36,7 +37,18 @@ def get_runtime_env(runtime_env: RuntimeEnvType) -> Union[DockerDeployment, Tmux
         return TmuxDeployment
 
 
-@app.post("/start", dependencies=[Depends(authenticate)])
+@app.get("/", include_in_schema=False)
+async def get_docs():
+    """Redirect to SwaggerUI"""
+    return RedirectResponse(url='/docs')
+
+
+@app.post(
+    "/start",
+    dependencies=[Depends(authenticate)],
+    name='Deploy a BentoML Model',
+    tags=['Operations'],
+)
 async def start(model_content: DeployModelInput):
     runtime_env = get_runtime_env(model_content.runtime_env)
     runtime_env_instance = runtime_env(model_content.model, model_content.version)
@@ -46,7 +58,12 @@ async def start(model_content: DeployModelInput):
     return Response(status_code=status.HTTP_200_OK, content=response)
 
 
-@app.post("/stop", dependencies=[Depends(authenticate)])
+@app.post(
+    "/stop",
+    dependencies=[Depends(authenticate)],
+    name='Undeploy a BentoML Model',
+    tags=['Operations'],
+)
 async def stop(model_content: UndeployModelInput):
     runtime_env = get_runtime_env(model_content.runtime_env)
     runtime_env_instance = runtime_env(model_content.model, model_content.version)
@@ -54,12 +71,13 @@ async def stop(model_content: UndeployModelInput):
     return Response(status_code=status.HTTP_200_OK, content=response)
 
 
-@app.get("/running")
+@app.get(
+    "/running",
+    name='List all deployed Models',
+    tags=['Information'],
+)
 async def running():
     return {
         'tmux': TmuxDeployment.get_running_models(),
         'docker': DockerDeployment.get_running_models(),
     }
-
-
-# ToDo: Log to MLflow/Page?
