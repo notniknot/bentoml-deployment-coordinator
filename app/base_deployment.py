43f28a41c0cf0b4fb1dc365523fcab428968f516
Niklas Scheidthauer <niklas.scheidthauer@generali.com>
2021-06-01 10:33:54 +0200

    Added do_not_track option

--- app/base_deployment.py
+++ app/base_deployment.py
@@ -1,7 +1,6 @@
 import logging
 import os
 import socket
-import sys
 
 from bentoml.yatai.client import get_yatai_client
 
@@ -10,6 +9,7 @@ from app.utils import get_config
 
 class Deployment:
     def __init__(self, model: str, version: str):
+        os.environ['BENTOML_DO_NOT_TRACK'] = 'True'
         self.logger = self.init_logger()
         self.logger.info(f'Initializing {type(self).__name__}: {model}:{version}')
         self.model = model
@@ -30,6 +30,9 @@ class Deployment:
     @classmethod
     def init_logger(self):
         logging.basicConfig(format='[%(asctime)s] %(levelname)s  %(name)s: %(message)s')
+        sql = logging.getLogger('sqlalchemy')
+        sql.setLevel(logging.ERROR)
+        sql.disabled = True
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.DEBUG)
         return logger
